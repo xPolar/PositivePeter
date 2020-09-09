bd2093e571e72b2a0422df198cbb9906d4573127
Restyled.io <commits@restyled.io>
2020-09-09 19:43:18 +0000

    Restyled by autopep8

--- Cogs/Prevention.py
+++ Cogs/Prevention.py
@@ -8,11 +8,12 @@ from discord.ext import commands
 
 import Config
 
+
 class Prevention(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
@@ -47,18 +48,19 @@ class Prevention(commands.Cog):
                         t.replace("don't", "do not")]
                     for variant in variants:
                         if variant in message.content.lower():
-                            await Config.CLUSTER["users"]["detections"].update_one({"_id": message.guild.id}, {"$inc": {"detections": 1}}, upsert = True)
+                            await Config.CLUSTER["users"]["detections"].update_one({"_id": message.guild.id}, {"$inc": {"detections": 1}}, upsert=True)
                             detections = await Config.CLUSTER["users"]["detections"].find_one({"_id": message.guild.id})
                             if detections["detections"] >= 3:
                                 embed = discord.Embed(
-                                    title = "Suicide Prevention",
-                                    description = f"Hey there {message.author.name}, based on your previous message I have detected hints of suicidal thoughts. If you are considering suicide please contact your local suicide prevention hotline, to find your hotline please visit [this](https://en.wikipedia.org/wiki/List_of_suicide_crisis_lines) website.",
-                                    color = Config.MAINCOLOR
+                                    title="Suicide Prevention",
+                                    description=f"Hey there {message.author.name}, based on your previous message I have detected hints of suicidal thoughts. If you are considering suicide please contact your local suicide prevention hotline, to find your hotline please visit [this](https://en.wikipedia.org/wiki/List_of_suicide_crisis_lines) website.",
+                                    color=Config.MAINCOLOR
                                 )
                                 hotline = await Config.CLUSTER["servers"]["hotlines"].find_one({"_id": message.guild.id})
                                 if hotline is not None:
-                                    embed.add_field(name = "**Server Side Hotline**", value = hotline["hotline"])
-                                await message.author.send(embed = embed)
+                                    embed.add_field(
+                                        name="**Server Side Hotline**", value=hotline["hotline"])
+                                await message.author.send(embed=embed)
                                 await Config.CLUSTER["users"]["detections"].delete_one({"_id": message.guild.id})
                             break
                 if message.guild is not None:
@@ -71,11 +73,11 @@ class Prevention(commands.Cog):
                         compliment_triggers = {"I'm worthless": ["No you're not!", "You're priceless", "You're worth more then diamonds!"],
                                                "I'm not good enough": "You're way more then enough", "Why am I not good enough?": "You're way more then enough",
                                                "I can't do anything right": ["You're doing just fine!", "You're perfect!"], "I suck": ["No you don't you're amazing!",
-                                               "Everyone has imperfections, you're great", "You're amazing"], "I'm sad": ["Don't be sad, you're great!"],
+                                                                                                                                       "Everyone has imperfections, you're great", "You're amazing"], "I'm sad": ["Don't be sad, you're great!"],
                                                "I'm trash": ["You're not trash, you're gold", "No you're not, you're amazing!"], "I'm dumb": ["No you're not!",
-                                               "You're one of the smartest people I know!", "You're extremely intilligent!"], "I'm an idiot": ["No you're not!",
-                                               "You're one of the smartest people I know!", "You're extremely intilligent!"], "I'm ugly": ["You're breathtaking!",
-                                               "No you're not, you're amazing!"]}
+                                                                                                                                              "You're one of the smartest people I know!", "You're extremely intilligent!"], "I'm an idiot": ["No you're not!",
+                                                                                                                                                                                                                                              "You're one of the smartest people I know!", "You're extremely intilligent!"], "I'm ugly": ["You're breathtaking!",
+                                                                                                                                                                                                                                                                                                                                          "No you're not, you're amazing!"]}
                         for trigger in compliment_triggers:
                             t = trigger.lower()
                             variants = [
@@ -90,6 +92,7 @@ class Prevention(commands.Cog):
                                         break
                                     else:
                                         await message.channel.send(compliment_triggers[trigger])
-                            
+
+
 def setup(bot):
     bot.add_cog(Prevention(bot))
